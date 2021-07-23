96a01f47b1c076b2a5873de4d11fee0f77534ade
Andrei-Loginov <andreyloginov1992@gmail.com>
2021-07-23 15:13:12 +0300

    Task 7

---
+++ Semester-5/C++/7_1/7_1.cpp
@@ -0,0 +1,25 @@
+﻿#include "header.h"
+
+
+int main(){
+    DAY days[NDAYS];
+
+    size_t size = read_days("input.txt", days);
+
+    DATE dat {24, 2, 2009};
+    //DATE dat{ 12, 12, 2005 };
+    //DATE dat{ 17, 1, 1996 };
+    //DATE dat{7, 7, 2015};
+    //DATE dat{7, 7, 2009};
+    //DATE dat{3, 9, 2025};
+    DAY* ptr = search({dat}, days, size, comp_days_date);
+
+    std::cout << "\nSearch by date ";
+    print_date(dat);
+
+    if (!ptr) std::cout << "\nNot found\n";
+    else print_day(*ptr);
+    
+
+    return 0;
+}
