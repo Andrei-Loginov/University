27931210f9ec25e3701a9eb4b8f302f1fbf36383
Andrei-Loginov <andreyloginov1992@gmail.com>
2021-07-23 15:47:55 +0300

    Reads short number with check for correct input

---
+++ Semester-5/C++/read_number/read_number.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+int main() {
+	char str[7], c;
+	char curr_len = 0;
+	while ((str[curr_len] = _getch()) != '\r' && curr_len < 6) {
+		if (isdigit(str[curr_len]) || (curr_len == 0 && str[curr_len] == '-'))
+			printf("%c", str[curr_len++]);
+	}
+	str[curr_len++] = '\0';
+	short num = atoi(str);
+	printf("\n%d", num);
+	return 0;
+}
